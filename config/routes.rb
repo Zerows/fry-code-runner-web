51ca5645f446f57042902679d01a0275b2897047
shrikanthkr <shrikanth_kr@yahoo.com>
2018-12-10 20:30:35 +0530

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  scope 'api/pads' , :controller => 'pads' do
-    get "/" , :action=> 'index', :as => 'pads_dashboard'
-    post "/", :action=>'create', :as => 'create_pad'
-    get '/:pad_id' ,:action => 'show_pad' , :as => 'show_pad'
-    put '/:pad_id' ,:action => 'update' , :as => 'update_pad'
-    delete '/:pad_id' ,:action => 'delete' , :as => 'delete_pad'
-  end
-
-  scope 'api/results' , :controller => 'results' do
-    get '/:result_id' ,:action => 'get_result' , :as => 'get_result'
-  end
-
   mount_ember_app :frontend, to: "/"
+
+  scope 'api' do
+    scope 'pads', controller: 'pads' do
+      get "/", :action => 'index', :as => 'pads_dashboard'
+      post "/", :action => 'create', :as => 'create_pad'
+      get '/:pad_id', :action => 'show_pad', :as => 'show_pad'
+      put '/:pad_id', :action => 'update', :as => 'update_pad'
+      delete '/:pad_id', :action => 'delete', :as => 'delete_pad'
+    end
+
+    scope 'results', controller: 'results' do
+      get '/:result_id', :action => 'get_result', :as => 'get_result'
+    end
+  end
 end
