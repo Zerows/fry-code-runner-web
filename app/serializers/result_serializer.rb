0a91c38e6e8596f9f024d1dd14c6de812f59770b
shrikanthkr <shrikanth_kr@yahoo.com>
2018-12-24 15:07:21 +0530

    add pad to result serializer always. Result should give pad which it is associated always.

--- app/serializers/result_serializer.rb
+++ app/serializers/result_serializer.rb
@@ -1,6 +1,6 @@
 class ResultSerializer < ActiveModel::Serializer
   include ActionView::Helpers::TextHelper
-  attributes :id, :status, :error, :html_output, :pad_id
+  attributes :id, :status, :error, :html_output, :pad
 
   def html_output
     simple_format(object.output)
