a8dedc4ec5ed6fba846cdcc690d79ea6b9e9fa25
shrikanthkr <shrikanth_kr@yahoo.com>
2018-12-25 17:51:12 +0530

    1. Simplify loader 2. Move update and create params separately.

--- frontend/app/components/load-spinner.js
+++ frontend/app/components/load-spinner.js
@@ -1,5 +1,20 @@
 import Component from '@ember/component';
+import { once } from '@ember/runloop'
 
 export default Component.extend({
-    
+    didReceiveAttrs() {
+        this._super(...arguments);
+        let size = this.get('size') || 10;
+        this.set('size', size);
+      },
+      didInsertElement(){
+        this.$().css('display', 'inline-block');
+        once('afterRender', () => {
+            let loaders = this.$('.loading-dots--dot')
+            let size = this.get('size');
+            loaders.height(size);
+            loaders.width(size);
+            loaders.css('border-radius', size/2 + "px");
+          });
+      }
 });
