fb2b6edcff8466824eba4ad2361519edaa0cccb9
shrikanthkr <shrikanth_kr@yahoo.com>
2019-01-03 21:21:53 +0530

    move reusable codes to mixins and use them in pads and questions

--- frontend/app/controllers/pads/show.js
+++ frontend/app/controllers/pads/show.js
@@ -1,52 +1,9 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object'
 import { later } from '@ember/runloop';
+import CodeRunner from '../../mixins/code-runner'
 
-export default Controller.extend({
-  init() {
-    this._super(...arguments)
-    this.set('supportedLanguages', ['java', 'javascript', 'python', 'ruby'])
-  },
-  isApiInProgress: function () {
-    let result = this.get('result');
-    let status = result != null ? result.get('status') : "";
-    if (status == 'in_queue'
-      || status == 'in_progress') {
-        return true;
-    } else {
-      return false;
-    }
-  },
-  showLoader: computed('result','saving', function () {
-    let finalVal = this.isApiInProgress() || this.get('saving');
-    return finalVal;
-  }),
-  saving: false,
-  maxPoll: 10,
-  poll(current = 0) {
-    let result = this.get('result');
-    later(() => {
-      result.reload().then((model) => {
-        this.notifyPropertyChange('result');
-        let canPoll = model.status == 'in_queue' || model.status == 'in_progress';
-        canPoll = canPoll && current <= this.get('maxPoll');
-        if (canPoll) {
-          this.poll(++current);
-        }
-        if(current >= this.get('maxPoll')){
-          model.set('status', 'cancelled');
-        }
-      })
-    }, 1000);
-  },
-  saveText: computed('saving', function (){
-    let result = this.get('saving');
-    if(result){
-      return 'Saving'
-    }else{
-      return 'Save'
-    }
-  }),
+export default Controller.extend(CodeRunner, {
   submitText: computed('result', function () {
     let result = this.get('result');
     let status = result != null ? result.get('status') : "";
