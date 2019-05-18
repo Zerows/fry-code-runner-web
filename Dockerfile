b1588a6be086c745ae2910bf93fe6335e622c9c9
shrikanthkr <shrikanth_kr@yahoo.com>
2019-05-18 15:31:45 -0700

    docker fixes

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM ruby:2.5
 WORKDIR /app
 COPY . /app
-RUN bundle
+ENV BUNDLE_PATH /box
 RUN curl -sL https://deb.nodesource.com/setup_8.x | bash -
 RUN apt-get install -y nodejs
 RUN npm install -g ember-cli
-RUN rake ember:install
 RUN cd frontend && npm install
 EXPOSE 3000
 EXPOSE 5672
--- dev_server_start.sh
+++ dev_server_start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-bundle install
+bundle check || bundle install
+echo "*********Gem check over*******"
+rm tmp/pids/server.pid
+RUN rake ember:install
 bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /app/bin/rails s -b 0.0.0.0 -p 3000 -e development
