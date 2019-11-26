bbe95202e46fe6ff46a34bdf9a3f59f9f674272a
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2019-11-26 16:07:01 -0300

    add general tests for models

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -4,4 +4,5 @@ require 'rails_helper'
 RSpec.describe Pad, type: :model do
   # Association test
   # ensure Pad model has a 1:m relationship with the Results model
+  it { should belong_to(:pad) }
 end
