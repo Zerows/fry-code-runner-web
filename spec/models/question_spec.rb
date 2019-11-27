6e73d0bf8208b0a4db81fc5d5b2e9c1da7e68a89
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2019-11-27 16:44:43 -0300

    add more tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
     it { should belong_to(:pad) }
+
+    it { should define_enum_for(:difficulty).with({easy: 0, medium: 1, hard: 3}) }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association tests
+  # Ensure Role model has a m:m relationship with the User model
+  it { should have_and_belong_to_many(:users) }
+  it { should belong_to(:resource) }
 end
