15baaf6922d857787934abddc0764eec037fc062
shrikanthkr <shrikanth_kr@yahoo.com>
2020-04-19 13:44:24 +0530

    fix runnng and showing for padas page. yet to do in questions

--- frontend/app/controllers/pads/show.js
+++ frontend/app/controllers/pads/show.js
@@ -1,71 +1,67 @@
 import Controller from '@ember/controller';
-import {computed} from '@ember/object'
-import CodeRunner from '../../mixins/code-runner'
-import {inject as service} from '@ember/service';
-import {run} from '@ember/runloop'
+import { computed } from '@ember/object'
+import { inject as service } from '@ember/service';
+import { run } from '@ember/runloop'
 
-export default Controller.extend(CodeRunner, {
+export default Controller.extend({
   session: service(),
   websockets: service('socket-io'),
   socket: null,
   canPublish: true,
   init() {
     this._super(...arguments);
-    const socket = this.websockets.socketFor('ws://192.168.1.226:4000');
-    this.set('socket', socket);
-    socket.on('connect', () => {
+    //const socket = this.websockets.socketFor('ws://192.168.1.226:4000');
+    //this.set('socket', socket);
+    // socket.on('connect', () => {
 
-    });
-    socket.on('event', (message) => {
-      var deltas = [];
-      deltas[0] = message;
-      run(() => {
-        this.set('canPublish', false);
-        this.editor.getSession().getDocument().applyDeltas(deltas);
-        this.set('canPublish', true);
-      });
-    });
-    socket.on('close', () => {
+    // });
+    // socket.on('event', (message) => {
+    //   var deltas = [];
+    //   deltas[0] = message;
+    //   run(() => {
+    //     this.set('canPublish', false);
+    //     this.editor.getSession().getDocument().applyDeltas(deltas);
+    //     this.set('canPublish', true);
+    //   });
+    // });
+    // socket.on('close', () => {
 
-    });
+    // });
 
   },
-  submitText: computed('result', function () {
-    let result = this.result;
-    let status = result != null ? result.get('status') : "";
-    if (status == 'in_queue') {
-      return "Submitting"
-    } else if (status == 'in_progress') {
-      return "In Progress"
-    } else if (status == 'cancelled') {
-      return "Retry";
+  saveText: computed('saving', function () {
+    if (this.saving) {
+      return 'Saving'
     } else {
-      return "Run";
+      return 'Save'
     }
+  }),
+  submitText: computed('result', 'polling', function () {
+    let result = this.result;
+    let status = result != null ? result.status : "";
+    let text = null
+    if (status == 'in_queue') {
+      text = "Submitting"
+    } else if (status == 'in_progress') {
+      text = "In Progress"
+    } else if (status == 'cancelled') {
+      text = "Retry";
+    } else {
+      text = "Run";
+    }
+    return text
+  }),
+
+  showLoader: computed('polling', 'saving', function () {
+    let finalVal = this.polling || this.saving;
+    return finalVal;
   }),
   actions: {
     submitPad(pad) {
-      //A temp record to show the status immesiately
-      let tempResult = this.store.createRecord('result', {
-        status: 'in_queue'
-      });
-      this.set('result', tempResult);
-      pad.submit().then((result) => {
-        //make sure we unload the temp record
-        tempResult.unloadRecord();
-        this.set('result', result);
-        this.poll();
-      }, () => {
-
-      });
+      this.send('submitPadAction', pad);
     },
     save(pad) {
-      this.set('saving', true);
-      pad.save().then(() => {
-        this.set('saving', false);
-      }, () => {
-        this.set('saving', false);
-      });
+      this.send('savePad', pad);
     },
     onEditorReady(editor) {
       this.set('editor', editor);
@@ -74,12 +70,12 @@ export default Controller.extend(CodeRunner, {
       this.model.set('content', val);
       run(() => {
         if (this.canPublish) {
-          this.socket.emit('event', event);
+          //this.socket.emit('event', event);
         }
       });
     },
     showQuestionModal() {
-      this.send('fetchQuestions', this);
+      this.send('fetchQuestions');
       this.set('showQuestions', true);
     },
     onQuestionsHidden() {
--- frontend/app/routes/pads/show.js
+++ frontend/app/routes/pads/show.js
@@ -1,16 +1,57 @@
 import Route from '@ember/routing/route';
 import GuestAuthenticatedRouteMixinMixin from 'frontend/mixins/guest-authenticated-route-mixin';
+import Poller from 'frontend/utils/poller'
 
 export default Route.extend(GuestAuthenticatedRouteMixinMixin, {
+  poller: Poller.create(),
   model(params) {
     return this.store.findRecord('pad', params.pad_id);
   },
-  setupController(controller,model){
+  setupController(controller, model) {
     controller.set('model', model);
+    controller.set('result', null);
+    controller.set('supportedLanguages', ['java', 'javascript', 'python', 'ruby'])
   },
   actions: {
-    fetchQuestions(controller){
-      controller.set('questions', this.store.findAll('question'));
-    }
+    async fetchQuestions() {
+      this.controller.set('questions', this.store.findAll('question'));
+    },
+    async submitPadAction(pad) {
+      try {
+        let tempResult = this.store.createRecord('result', {
+          status: 'in_queue'
+        });
+        this.controller.set('result', tempResult);
+        let result = await pad.submit()
+        tempResult.unloadRecord();
+        this.controller.set('result', null);
+        this.poll(result)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async savePad(pad) {
+      this.controller.set('saving', true);
+      try {
+        await pad.save()
+        this.controller.set('saving', false);
+      } catch (error) {
+        this.controller.set('saving', false);
+      }
+    },
+  },
+  async poll(currentResult) {
+    this.poller.startPoll(async () => {
+      this.controller.set('polling', true)
+      let newResult = await currentResult.reload()
+      let canPoll = newResult.status == 'in_queue' || newResult.status == 'in_progress';
+      this.controller.set('result', newResult);
+      this.controller.set('polling', canPoll)
+      return !canPoll
+    }, () => {
+      let result = this.controller.result
+      this.controller.set('polling', false)
+      result.set('status', 'cancelled');
+    })
   }
 });
