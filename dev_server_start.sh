bbce92ac1e35d4e5e2b54c76e1a8ba47d03ea959
shrikanthkr <shrikanth_kr@yahoo.com>
2020-04-21 15:59:48 +0530

    bring sockets into compose

--- dev_server_start.sh
+++ dev_server_start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 rm tmp/pids/server.pid
+bundle install
+bundle exec rake ember:install
+( cd frontend && npm install )
 bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /app/bin/rails s -b 0.0.0.0 -p 3000 -e development
