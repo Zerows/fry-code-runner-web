39afa0eba10629819b17947bc8fe4fe7b0a24f63
shrikanthkr <shrikanth_kr@yahoo.com>
2020-04-25 17:09:30 +0530

    fix showing isAuth

--- frontend/app/controllers/pads/show.js
+++ frontend/app/controllers/pads/show.js
@@ -1,7 +1,9 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object'
+import {computed} from '@ember/object'
+import {inject as service} from '@ember/service';
 
 export default Controller.extend({
+  session: service(),//used in hbs for authenticated
   saveText: computed('saving', function () {
     if (this.saving) {
       return 'Saving'
