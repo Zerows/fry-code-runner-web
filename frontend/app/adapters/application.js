9e1f00c8e43ee553c7a640320538c0e694dca04c
shrikanthkr <shrikanth_kr@yahoo.com>
2020-04-28 16:03:14 +0530

    just a try

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,13 +1,13 @@
 import RESTAdapter from '@ember-data/adapter/rest';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
 
 export default RESTAdapter.extend({
   host: window.location.origin,
   namespace: 'api',
   session: service(),
-  headers: computed(function () {
-    let { token } = this.session.data.authenticated;
+  headers: computed('session.data.authenticated', function () {
+    let {token} = this.session.data.authenticated;
     if (token !== undefined) {
       return {
         'Authorization': `Bearer ${token}`,
