1aa10d7369a5837b12539794820cca7e80219399
shrikanthkr <shrikanth_kr@yahoo.com>
2020-05-01 16:30:21 +0530

    precompile

--- server_start.sh
+++ server_start.sh
@@ -3,4 +3,5 @@ bundle install --without test development
 bundle exec rake ember:install
 cd frontend && npm install
 echo "Starting rails"
+bundle exec rake assets:precompile
 bundle exec /app/bin/rails s -e production
