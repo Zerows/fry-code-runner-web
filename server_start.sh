0b8bfd7e53cefc1490b00a15e67d8e58615b3f00
shrikanthkr <shrikanth_kr@yahoo.com>
2020-05-01 16:52:40 +0530

    fix command issue

--- server_start.sh
+++ server_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 bundle install --without test development
 bundle exec rake ember:install
-cd frontend && npm install
+(cd frontend && npm install)
 echo "Starting rails"
 bundle exec rake assets:precompile
 bundle exec /app/bin/rails s -e production
