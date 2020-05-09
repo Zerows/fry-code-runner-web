3ff42970c803ea634e795ad93e68a08a37589916
shrikanthkr <shrikanth_kr@yahoo.com>
2020-05-09 18:26:01 +0530

    fix cookie domain error and increment the es lint

--- frontend/app/session-stores/application.js
+++ frontend/app/session-stores/application.js
@@ -2,5 +2,9 @@ import Cookie from 'ember-simple-auth/session-stores/cookie';
 import config from 'frontend/config/environment';
 
 export default class ApplicationSessionStore extends Cookie {
-  cookieDomain = config.cookieDomain;
+
+  init(...args) {
+    super.init(...args);
+    this.cookieDomain = config.cookieDomain;
+  }
 }
